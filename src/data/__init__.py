d760f6bd77fe4854dca095eeef371743fbb89723
Henrique <hrqneto@gmail.com>
2025-03-17 11:30:06 -0300

    chore: adiciona src/data ao .gitignore

--- src/data/__init__.py
+++
@@ -1,5 +0,0 @@
-DATA_FILES = ["products.csv", "products.json", "large_products.csv", "dataset.csv"]
-
-def list_data_files():
-    """Retorna a lista de arquivos disponíveis na pasta data."""
-    return DATA_FILES
