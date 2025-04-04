da0105c128a005b9a6b4fa22c065fb2c4dabb8b4
Henrique <hrqneto@gmail.com>
2025-04-04 17:48:38 -0300

    chore(init): ajustes no módulo de serviços

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -4,7 +4,11 @@ from .upload_service import process_and_index_csv
 from .weaviate_client import create_weaviate_client
 from .autocomplete_service import get_autocomplete_suggestions
 
-# Expõe essas funções quando importarem "services"
+# Serviços auxiliares (não precisa expor se usados só internamente)
+# from .validation_service import validar_produto
+# from .image_service import processar_e_enviar_imagem
+# from .report_service import salvar_relatorio_erros
+
 __all__ = [
     "index_products",
     "search_products",
